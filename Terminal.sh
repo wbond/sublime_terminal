8d69c9da01feea78a2151c72869ed06739454af8
wbond <will@wbond.net>
2011-08-08 00:03:34 -0400

    Fixed Terminal.sh to not launch a second window on Lion

--- Terminal.sh
+++ Terminal.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 CD_CMD="cd "\\\"$(pwd)\\\"" && clear"
+VERSION=$(sw_vers -productVersion)
+if (( $(expr $VERSION '<' 10.7.0) )); then
+	IN_WINDOW="in window 1"
+fi
 osascript<<END
 try
 	tell application "System Events"
 		if (count(processes whose name is "Terminal")) is 0 then
 			tell application "Terminal"
 				activate
-				do script "$CD_CMD" in window 1
+				do script "$CD_CMD" $IN_WINDOW
 			end tell
 		else
 			tell application "Terminal"
