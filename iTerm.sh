99598d0be0db7d3111630b0c5d5236d60d1f6702
Travis Warlick <twarlick@gmail.com>
2013-06-25 10:44:42 -0400

    Simplify iTerm.sh and fix for OSX Mountain Lion

--- iTerm.sh
+++ iTerm.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 
-CD_CMD="cd "\\\"$(pwd)\\\"" && clear"
-VERSION=$(sw_vers -productVersion)
+###
+# See Notice.md for attribution
+###
 
-if (( $(expr $VERSION '<' 10.7) )); then
-	RUNNING=$(osascript<<END
-	tell application "System Events"
-	    count(processes whose name is "iTerm")
-	end tell
-END
-)
-else
-	RUNNING=1
-fi
+CD_PATH="$(pwd)"
 
-if (( $RUNNING )); then
-	osascript<<END
-	tell application "iTerm"
-		activate
-		set term to (make new terminal)
-		tell term
-			set sess to (launch session "Default Session")
-			tell sess
-				write text "$CD_CMD"
-			end tell
-		end tell
-	end tell
+osascript<<END
+set myPath to quoted form of POSIX path of "$CD_PATH"
+
+tell application "iTerm"
+  activate
+  if the (count of terminal) is 0 then
+    set myTerm to make new terminal
+  else
+    set myTerm to the current terminal
+  end if
+  tell myTerm
+    launch session "Default Session"
+    tell the last session to write text "cd " & myPath
+  end tell
+end tell
 END
-else
-	osascript<<END
-	tell application "iTerm"
-		activate
-		set sess to the first session of the first terminal
-		tell sess
-			write text "$CD_CMD"
-		end tell
-	end tell
-END
-fi
