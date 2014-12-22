181b445f8a1dc69c94ad57d05b1e309c20710580
Todd Wolfson <todd@twolfson.com>
2014-12-21 19:47:43 -0600

    Added support for Linux Mint/Cinnamon. Fixes #61

--- Terminal.py
+++ Terminal.py
@@ -84,10 +84,10 @@ class TerminalSelector():
 
         else:
             ps = 'ps -eo comm | grep -E "gnome-session|ksmserver|' + \
-                'xfce4-session" | grep -v grep'
+                'xfce4-session|cinnamon-sessio" | grep -v grep'
             wm = [x.replace("\n", '') for x in os.popen(ps)]
             if wm:
-                if wm[0] == 'gnome-session':
+                if wm[0] == 'gnome-session' or wm[0] == 'cinnamon-sessio':
                     default = 'gnome-terminal'
                 elif wm[0] == 'xfce4-session':
                     default = 'terminal'
