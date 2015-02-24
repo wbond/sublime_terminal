e8db29588c4b2ee9d16959adfccf7df2bdc551e4
Vitor Barbosa <vitorhsb@gmail.com>
2015-02-24 16:34:49 +0000

    Ability to open a new Tab in current iTerm window
    A new parameter can be used to instruct to open a new tab in the
    current window or to open a new window altogether. Also, a few bug
    corrections on the RUNNING variable.

--- iTerm.sh
+++ iTerm.sh
@@ -2,11 +2,22 @@
 
 CD_CMD="cd "\\\"$(pwd)\\\"" && clear"
 VERSION=$(sw_vers -productVersion)
+NEWWINDOW=0
+
+while [ "$1" != "" ]; do
+	case $1 in
+		--openNewWindow )   NEWWINDOW=1
+												;;
+		--openNewTab )      NEWWINDOW=0
+												;;
+	esac
+	shift
+done
 
 if (( $(expr $VERSION '<' 10.7) )); then
 	RUNNING=$(osascript<<END
 	tell application "System Events"
-	    count(processes whose name is "iTerm")
+			count(processes whose name is "iTerm")
 	end tell
 END
 )
@@ -14,27 +25,40 @@ else
 	RUNNING=1
 fi
 
-if (( $RUNNING )); then
+if (( ! $RUNNING )); then
 	osascript<<END
 	tell application "iTerm"
-		activate
-		set term to (make new terminal)
-		tell term
-			set sess to (launch session "Default Session")
-			tell sess
+		tell current terminal
+			launch session "Default Session"
+			tell the last session
 				write text "$CD_CMD"
 			end tell
 		end tell
 	end tell
 END
 else
-	osascript<<END
-	tell application "iTerm"
-		activate
-		set sess to the first session of the first terminal
-		tell sess
-			write text "$CD_CMD"
+	if (( $NEWWINDOW )); then
+		osascript &>/dev/null <<EOF
+		tell application "iTerm"
+			set term to (make new terminal)
+			tell term
+				launch session "Default Session"
+				tell the last session
+					write text "$CD_CMD"
+				end tell
+			end tell
 		end tell
-	end tell
-END
+EOF
+	else
+		osascript &>/dev/null <<EOF
+		tell application "iTerm"
+			tell current terminal
+				launch session "Default Session"
+				tell the last session
+					write text "$CD_CMD"
+				end tell
+			end tell
+		end tell
+EOF
+	fi
 fi
