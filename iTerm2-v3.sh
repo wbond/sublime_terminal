785463f4ff560591a6cbe56ab2ac9da79bbc227c
James Stidard <james@stidard.com>
2019-02-09 17:59:05 +0000

    hacked together

--- iTerm2-v3.sh
+++ iTerm2-v3.sh
@@ -6,6 +6,7 @@ if echo "$SHELL" | grep -E "/fish$" &> /dev/null; then
 fi
 VERSION=$(sw_vers -productVersion)
 OPEN_IN_TAB=0
+EXTRA_CMD=0
 
 while [ "$1" != "" ]; do
 	PARAM="$1"
@@ -14,9 +15,22 @@ while [ "$1" != "" ]; do
 		--open-in-tab)
 			OPEN_IN_TAB=1
 			;;
+		--command)
+			EXTRA_CMD=$VALUE
+			;;
 	esac
 	shift
 done
+
+if [ -n "$EXTRA_CMD" ]; then
+	echo "hi"
+	CD_CMD="$CD_CMD && $EXTRA_CMD"
+	if echo "$SHELL" | grep -E "/fish$" &> /dev/null; then
+		CD_CMD="$CD_CMD; and $EXTRA_CMD"
+	fi
+fi
+
+echo $CD_CMD
 
 if (( $(expr $VERSION '<' 10.7) )); then
 	RUNNING=$(osascript<<END
